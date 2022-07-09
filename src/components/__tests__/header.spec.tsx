940ff21de4ced8a2c66733d44b0b8c525007a3ea
José Murillo <jooseemurillo@gmail.com>
2022-07-09 14:23:39 -0300

    fix name describe test

--- src/components/__tests__/header.spec.tsx
+++ src/components/__tests__/header.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen} from '@testing-library/react';
 import { Header } from '../Header';
 
-describe('<Hearder />', () => {
+describe('<Header />', () => {
   it('Render component correctly', () => {
     render(<Header />);
     expect(screen.getByPlaceholderText('Adicionar uma nova tarefa')).toBeInTheDocument();
