c52f4dee492ab3fcb03b1d7dd869d8c1be81ba3f
José Murillo <jooseemurillo@gmail.com>
2022-07-08 21:18:09 -0300

    create empty

---
+++ src/components/Empty.tsx
@@ -0,0 +1,15 @@
+import styles from '../styles/components/empty.module.css';
+
+import clipboardImage from '../assets/clipboard.svg';
+
+export function Empty () {
+  return (
+    <div className={styles.container}>
+      <img src={clipboardImage} />
+      <span className={styles.title}>
+        Você ainda não tem tarefas cadastradas
+        Crie tarefas e organize seus itens a fazer
+      </span>
+    </div>
+  );
+}
