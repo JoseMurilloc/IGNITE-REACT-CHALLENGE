3630dc31e0939b6cd536074894ac71f7b19443d0
José Murillo <jooseemurillo@gmail.com>
2022-07-09 21:27:33 -0300

    add create new task

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,34 @@
+import { useState } from 'react';
 import styles from '../styles/components/header.module.css';
 import logoImage from '../assets/logo.svg';
 import { PlusCircle } from 'phosphor-react';
+import { Task } from '../pages/App';
+import { TaskService } from '../services/task';
 
-export function Header() {
+type Props = {
+  onTasks: React.Dispatch<React.SetStateAction<Task[]>>
+}
+
+export function Header({onTasks}: Props) {
+  const [title, setTitle] = useState('')
+  const [isSubmit, setIsSubmit] = useState(false)
+  
+  const handleCreateNewTask = async () => {
+    setIsSubmit(true)
+    const {status, newTask} = await TaskService.createNewTask(title)
+
+    if (!status) {
+      return
+    }
+
+    onTasks((oldTasks: Task[]) => [...oldTasks, newTask])
+    setTitle('')
+    setIsSubmit(false)
+  }
+
+
+
+  
   return (
     <header className={styles.container}>
       <img src={logoImage} alt="Logo da aplicação todo" />  
@@ -11,10 +37,18 @@ export function Header() {
           className={styles.inputSearch} 
           type="text" 
           placeholder="Adicionar uma nova tarefa"
+          onChange={e => setTitle(e.target.value)}
+          value={title}
         />
-        <button>
-          <span>Criar</span>
-          <PlusCircle color="white" size={20} />
+        <button 
+          onClick={handleCreateNewTask} 
+          disabled={isSubmit}
+        >
+          Criar
+          <PlusCircle 
+            color={isSubmit ? '#bfbfbf' : '#ffffff'} 
+            size={20} 
+          />
         </button>
       </div>   
     </header>
