3472e923cf0e833bcf2f3740efb8a45febe1a93a
José Murillo <jooseemurillo@gmail.com>
2022-07-09 21:26:15 -0300

    fetching all tasks and list

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { CardTask } from '../components/CardTask';
 import { Empty } from '../components/Empty';
 import { Header } from '../components/Header';
+import { TaskService } from '../services/task';
 import styles from '../styles/pages/app.module.css';
 
-type Task = {
+export type Task = {
   id: number;
   complete: boolean;
   title: string;
 }
 
 function App() {
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  useEffect(() => {
+    fetchingAllTasks();   
+  }, [])
+
+  const tasksComplete = tasks.filter(task => task.complete).length;
+
+  const fetchingAllTasks = async () => {
+    const response = await TaskService.getTasks()
+
+    if (!response.status) {
+      return;
+    }
+
+    setTasks(response.tasks)
+  }
 
   return (
     <div className={styles.container}>
-      <Header />
+      <Header onTasks={setTasks}/>
       <main className={styles.main}>
         <section className={styles.info}>
           <aside className={styles.infoTask}>
             <span>Tarefas criadas</span>
             <div className={styles.circle}>
-              <span>0</span>
+              <span>{tasks.length}</span>
             </div>
           </aside>
 
           <aside className={styles.infoTask}>
             <span>Concluídas</span>
             <div>
-              <span className={styles.counter}>2 de 5</span>
+              <span className={styles.counter}>
+                {`${tasksComplete} de ${tasks.length}`}
+              </span>
             </div>
           </aside>
         </section>
@@ -36,7 +55,13 @@ function App() {
         {!tasks.length && <Empty />}
         
         <div className={styles.listTasks}>
-          {tasks.map(task => <CardTask key={task.id} task={task}/>)} 
+          {tasks.map(task => 
+            <CardTask 
+              key={task.id} 
+              task={task}
+              onTasks={setTasks}
+            />
+          )} 
         </div>
       </main>
     </div>
