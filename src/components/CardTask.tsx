3f10d6c486b7843afa8935dc106e235ca2e88a06
José Murillo <jooseemurillo@gmail.com>
2022-07-09 21:29:58 -0300

    add handle delete and complete task

--- src/components/CardTask.tsx
+++ src/components/CardTask.tsx
@@ -1,23 +1,58 @@
 import styles from '../styles/components/cardTask.module.css';
 import { Trash, Circle, CheckCircle } from 'phosphor-react';
+import { TaskService } from '../services/task';
+import { Task } from '../pages/App';
 
 
 type Props = {
-  task: {
-    id: number;
-    complete: boolean;
-    title: string;
-  }
+  task: Task;
+  onTasks: React.Dispatch<React.SetStateAction<Task[]>>;
 }
 
-export function CardTask ({task}: Props) {
+export function CardTask ({task, onTasks}: Props) {
+
+  const handleDeleteTask = async (taskId: number) => {
+    const {status} = await TaskService.deleteTask(taskId);
+
+    if (!status) {
+      return;
+    }
+
+    onTasks((oldTasks: Task[]) => oldTasks.filter(task => task.id !== taskId))
+  }
+
+  const handleCompleteTask = async (taskId: number) => {
+    const {status, updatedTask} = await TaskService.completeTask(taskId, task.complete);
+
+    if (!status) {
+      return;
+    }
+
+    onTasks((oldTasks: Task[]) => oldTasks.map(task => 
+        task.id === taskId ?
+          updatedTask : task
+    ))
+  }
+
   return (
     <div className={styles.container}>
       {task.complete 
-        ? <CheckCircle color="#4EA8DE" size={20} /> 
-        : <Circle color="#4EA8DE" size={20} />}
-      <span>{task.title}</span>
-      <Trash color="#808080" size={20} />
+        ? (
+          <button onClick={() => handleCompleteTask(task.id)}>
+            <CheckCircle color="#4EA8DE" size={20} />
+          </button>
+        ) 
+        : (
+          <button onClick={() => handleCompleteTask(task.id)}>
+            <Circle color="#4EA8DE" size={20} />
+          </button>
+        )}
+      <div>
+        <span>{task.title}</span>
+      </div>
+      <button onClick={() => handleDeleteTask(task.id)}>
+        <Trash color="#808080" size={20} />
+      </button>
     </div>
   )
 }
