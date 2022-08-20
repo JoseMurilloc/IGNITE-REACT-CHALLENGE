260c9674f21a1bfb5d3544973d88b2a4ca963f49
José Murillo <jooseemurillo@gmail.com>
2022-08-20 12:13:29 -0300

    test: add test to header when create new todo

--- todo-react/src/components/__tests__/header.spec.tsx
+++ todo-react/src/components/__tests__/header.spec.tsx
@@ -10,13 +10,27 @@ describe('<Header />', () => {
     ).toBeInTheDocument()
   })
 
-  it('Submit search input correctly', async () => {
+  it('should it is with search input clear after the click button create', async () => {
     render(<Header onTasks={jest.fn} />)
     const inputSearch = screen.getByRole('textbox')
     userEvent.type(inputSearch, 'New task of todo')
 
+    const buttonCreateTODO = screen.getByRole('button')
+    await userEvent.click(buttonCreateTODO)
+
     await waitFor(() => {
-      expect(inputSearch).toHaveValue('New task of todo')
+      expect(inputSearch).toHaveValue('')
+    })
+  })
+
+  it('should be have not permitted submit create new tudo with input clear', async () => {
+    render(<Header onTasks={jest.fn} />)
+
+    const buttonCreateTODO = screen.getByRole('button')
+    await userEvent.click(buttonCreateTODO)
+
+    await waitFor(() => {
+      expect(screen.getByPlaceholderText('Campo obrigatório')).toHaveValue('')
     })
   })
 })
