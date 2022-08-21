30ef2cef11c30134176854e1d184071bedf999a6
José Murillo <jooseemurillo@gmail.com>
2022-08-21 11:20:06 -0300

    (todo) add prop to get by id

--- todo-react/src/components/Empty.tsx
+++ todo-react/src/components/Empty.tsx
@@ -6,7 +6,7 @@ export function Empty() {
   return (
     <div className={styles.container}>
       <img src={clipboardImage} alt="logo" />
-      <span className={styles.title}>
+      <span data-testid="message-empty" className={styles.title}>
         Você ainda não tem tarefas cadastradas Crie tarefas e organize seus
         itens a fazer
       </span>
