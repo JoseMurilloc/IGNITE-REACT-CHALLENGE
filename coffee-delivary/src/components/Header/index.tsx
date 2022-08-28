dfc0d211e2b96fee4ebaaa494734f20c5364d673
José Murillo <jooseemurillo@gmail.com>
2022-08-28 19:40:49 -0300

    (coffee) ad amount coffees in cart on header

--- coffee-delivary/src/components/Header/index.tsx
+++ coffee-delivary/src/components/Header/index.tsx
@@ -2,8 +2,14 @@ import styles from './styles.module.css';
 import logoCoffeeDelivery from '../../assets/logo.svg'
 import { MapPin, ShoppingCart } from 'phosphor-react';
 import { Link } from 'react-router-dom';
+import { useCart } from '../../hooks/CartContext';
+import { useMemo } from 'react';
 
 export function Header () {
+  
+  const {coffees} = useCart();
+  const amountCoffeeOfCart = useMemo(() => coffees.length, [coffees]);
+
   return (
     <header className={styles.containerHeader}>
       <div className={styles.content}>
@@ -16,9 +22,14 @@ export function Header () {
             <MapPin size={22} weight="fill" />
             <span>Porto alegre</span>
           </button>
-          <button className={styles.actionCart}>
+          <Link to='/Checkout' className={styles.actionCart}>
+            {!!amountCoffeeOfCart &&  (
+              <aside className={styles.counter}>
+                {amountCoffeeOfCart}
+              </aside>
+            )}
             <ShoppingCart color="#C47F17" size={22} weight="fill" />
-          </button>
+          </Link>
         </div>
       </div>
     </header>
