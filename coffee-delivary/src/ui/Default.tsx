6cba204f4b286ed5e4947d4383290a1acef3d5de
José Murillo <jooseemurillo@gmail.com>
2022-08-28 19:40:05 -0300

    (coffee) wrapper cart provider

--- coffee-delivary/src/ui/Default.tsx
+++ coffee-delivary/src/ui/Default.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
+import { CartContextProvider } from "../hooks/CartContext";
 
 export function Default () {
   return (
-    <>
+    <CartContextProvider>
       <Header />
       <Outlet />
-    </>
+    </CartContextProvider>
   );
 }
