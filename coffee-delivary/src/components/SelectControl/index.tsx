573fca8fc12f2bc1290f4dd748be3e8b06992a9f
José Murillo <jooseemurillo@gmail.com>
2022-09-07 15:39:46 -0300

    (coffee) create selector of state

---
+++ coffee-delivary/src/components/SelectControl/index.tsx
@@ -0,0 +1,12 @@
+import { STATES } from '../../util/fetchingAllStates';
+import styles from './styles.module.css';
+
+export function SelectControl ({...rest}) {
+  return (
+    <select className={styles.containerSelectedControl} {...rest}>
+      {STATES.map(state => (
+        <option value={state.uf}>{state.uf}</option>
+      ))}
+    </select>
+  )
+}
