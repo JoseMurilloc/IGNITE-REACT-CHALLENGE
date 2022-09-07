b9d815467af83e99ae7ca06691dcfc3248b984d2
José Murillo <jose.nunes@pede.ai>
2022-09-07 12:36:03 -0300

    create skeleton of cart

---
+++ coffee-delivary/src/components/Cart/index.tsx
@@ -0,0 +1,23 @@
+import styles from './styles.module.css';
+
+export function Cart () {
+  return (
+    <div className={styles.containerCartCoffee}>
+      <footer>
+        <div className={styles.infoLine}>
+          <span>Total de itens</span>
+          <span>R$ 29,70</span>
+        </div>
+        <div className={styles.infoLine}>
+          <span>Entrega</span>
+          <span>R$ 3,50</span>
+        </div>
+        <div className={styles.infoLine}>
+          <span>Total</span>
+          <span>R$ 33,20</span>
+        </div>
+      </footer>
+      <button>Confirmar Pedido</button>
+    </div>
+  )
+}
