1418f8bb30422e4bfedcc86b737705420a28756e
José Murillo <jose.nunes@pede.ai>
2022-09-07 12:46:18 -0300

    (coffee) start checkout page

--- coffee-delivary/src/pages/Checkout/index.tsx
+++ coffee-delivary/src/pages/Checkout/index.tsx
@@ -1,3 +1,31 @@
+import { FormProvider, useForm } from 'react-hook-form';
+import { Cart } from '../../components/Cart';
+import { FormCart } from '../../components/FormCart';
+import styles from './styles.module.css';
+
 export function Checkout () {
-  return <h1>Checkout</h1>;
+
+  const newBuyForm = useForm()
+  const { handleSubmit } = newBuyForm
+
+  async function handleConfirmPayment(data: any) {
+    console.log({data})
+  }
+
+  return (
+    <main className={styles.containerCheckout}>
+      <form onSubmit={handleSubmit(handleConfirmPayment)}>
+        <section className={styles.sectionInfoOfPurchase}>
+          <h2>Complete seu pedido</h2>
+          <FormProvider {...newBuyForm}>
+            <FormCart />
+          </FormProvider>
+        </section>
+        <section>
+          <h2>Cafés selecionados</h2>
+          <Cart />
+        </section>
+      </form>
+    </main>
+  );
 }
