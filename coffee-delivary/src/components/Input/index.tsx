f26c3d593e92ff315495b42e97b53ab838c35bd7
José Murillo <jooseemurillo@gmail.com>
2022-09-07 15:24:45 -0300

    (coffee)  create input using hook form

---
+++ coffee-delivary/src/components/Input/index.tsx
@@ -0,0 +1,14 @@
+import { InputHTMLAttributes } from 'react';
+import { FormProvider, useFormContext } from 'react-hook-form';
+import styles from './styles.module.css';
+
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  registerName: string;
+}
+
+export function Input ({registerName, ...rest}: Props) {
+  const {register} = useFormContext()
+  return (
+    <input {...register(registerName)} className={styles.containerInputControl} {...rest}/>
+  );
+}
