2daf1b01d66da14c8272513031c20aa8f7037666
José Murillo <jose.nunes@pede.ai>
2022-09-07 12:34:04 -0300

    (coffee) create select payment button

---
+++ coffee-delivary/src/components/SelectPaymentButton/index.tsx
@@ -0,0 +1,21 @@
+import { Bank, CreditCard, Money } from 'phosphor-react';
+import { ButtonHTMLAttributes } from 'react';
+import { MethodsPayment } from '../FormCart/types';
+import styles from './styles.module.css';
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+  label: string;
+  typePayment: MethodsPayment
+  selected: boolean;
+}
+
+export function SelectPaymentButton ({label, typePayment: type, selected=false, ...rest}: Props) {
+  return (
+    <button type="button" className={selected ? styles.containerSelectPaymentButtonSelected : styles.containerSelectPaymentButton} {...rest}>
+      {type === 'debit' && <Bank size="2rem" color="#8047F8" />}
+      {type === 'money' && <Money size="2rem" color="#8047F8" />}
+      {type === 'credit'  && <CreditCard size="2rem" color="#8047F8" />}
+      <span>{label}</span>
+    </button>
+  );
+}
