218dd1672659950e30782e7f7307174f1495a44d
José Murillo <jooseemurillo@gmail.com>
2022-09-11 21:46:39 -0300

    (coffee) build UI list coffees of cart

--- coffee-delivary/src/components/Cart/index.tsx
+++ coffee-delivary/src/components/Cart/index.tsx
@@ -1,23 +1,51 @@
+import { Minus, Plus, ShoppingCart, Trash } from 'phosphor-react';
+import { useCart } from '../../hooks/CartContext';
 import styles from './styles.module.css';
 
 export function Cart () {
+  const {coffees} = useCart()
   return (
     <div className={styles.containerCartCoffee}>
+      <main>
+        {coffees.map(coffee => (
+          <>
+            <aside className={styles.coffee}>
+              <aside className={styles.infoCoffee}>
+                <img src={coffee.image} alt="image-coffee" />
+                <div className={styles.detailsCoffee}>
+                  <div className={styles.actionsCart}>
+                    <h1>{coffee.title}</h1>
+                    <div className={styles.addonsCoffeeCart}>
+                      <button onClick={() => {}} className={styles.buttonMinus}>
+                        <Minus color="#8047F8" size="1.5rem" weight="fill"/>
+                      </button>
+                      <aside className={styles.counter}>1</aside>
+                      <button onClick={() => {}} className={styles.buttonPlus}>
+                        <Plus color="#8047F8" size="1.5rem" weight="fill"/>
+                      </button>
+                      <button
+                        onClick={() => {}}
+                        className={styles.buttonRemoveCart}>
+                          <Trash color="#8047F8" size="2.2rem" />
+                          <span>Remover</span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </aside>
+              <div className={styles.price}>
+                <span>R$ 100,12</span>
+              </div>
+            </aside>
+            <div className={styles.separator} />
+          </>
+        ))}
+      </main>
       <footer>
-        <div className={styles.infoLine}>
-          <span>Total de itens</span>
-          <span>R$ 29,70</span>
-        </div>
-        <div className={styles.infoLine}>
-          <span>Entrega</span>
-          <span>R$ 3,50</span>
-        </div>
-        <div className={styles.infoLine}>
-          <span>Total</span>
-          <span>R$ 33,20</span>
-        </div>
+          <button className={styles.confirmOrder}>
+            <span>Confirmar Pedido</span>
+          </button>
       </footer>
-      <button type="submit">Confirmar Pedido</button>
     </div>
   )
 }
