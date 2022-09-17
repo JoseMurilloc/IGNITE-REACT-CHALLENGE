132de1e23c1409190b26eac9d3ce4c141e2545b3
José Murillo <jooseemurillo@gmail.com>
2022-09-16 23:22:54 -0300

    (coffee) list and calcule total price

--- coffee-delivary/src/components/Cart/index.tsx
+++ coffee-delivary/src/components/Cart/index.tsx
@@ -1,48 +1,73 @@
 import { Minus, Plus, ShoppingCart, Trash } from 'phosphor-react';
+import { useMemo, useState } from 'react';
 import { useCart } from '../../hooks/CartContext';
+import { formatNumber } from '../../util/formatNumber';
 import styles from './styles.module.css';
 
 export function Cart () {
-  const {coffees} = useCart()
+
+  const {coffees,removeCoffeeCart} = useCart()
+  const [deliveryValue, setDeliveryValue] = useState(3)
+
+  const totalPrice = useMemo(() =>
+    coffees.reduce((sumTotal, coffee) => sumTotal += (coffee.price * coffee.amount), 0)
+  , [coffees])
+
+
+
+
   return (
     <div className={styles.containerCartCoffee}>
       <main>
         {coffees.map(coffee => (
-          <>
-            <aside className={styles.coffee}>
-              <aside className={styles.infoCoffee}>
-                <img src={coffee.image} alt="image-coffee" />
-                <div className={styles.detailsCoffee}>
-                  <div className={styles.actionsCart}>
-                    <h1>{coffee.title}</h1>
-                    <div className={styles.addonsCoffeeCart}>
-                      <button onClick={() => {}} className={styles.buttonMinus}>
-                        <Minus color="#8047F8" size="1.5rem" weight="fill"/>
-                      </button>
-                      <aside className={styles.counter}>1</aside>
-                      <button onClick={() => {}} className={styles.buttonPlus}>
-                        <Plus color="#8047F8" size="1.5rem" weight="fill"/>
-                      </button>
-                      <button
-                        onClick={() => {}}
-                        className={styles.buttonRemoveCart}>
-                          <Trash color="#8047F8" size="2.2rem" />
-                          <span>Remover</span>
-                      </button>
-                    </div>
+         <>
+          <aside className={styles.coffee}>
+            <aside className={styles.infoCoffee}>
+              <img src={coffee.image} alt="image-coffee" />
+              <div className={styles.detailsCoffee}>
+                <div className={styles.actionsCart}>
+                  <h1>{coffee.title}</h1>
+                  <div className={styles.addonsCoffeeCart}>
+                    <button type="button" onClick={() => {}} className={styles.buttonMinus}>
+                      <Minus color="#8047F8" size="1.5rem" weight="fill"/>
+                    </button>
+                    <aside className={styles.counter}>{coffee.amount}</aside>
+                    <button type="button" onClick={() => {}} className={styles.buttonPlus}>
+                      <Plus color="#8047F8" size="1.5rem" weight="fill"/>
+                    </button>
+                    <button onClick={() => removeCoffeeCart(coffee.id)} type="button" className={styles.buttonRemoveCart}>
+                        <Trash color="#8047F8" size="2.2rem" />
+                        <span>Remover</span>
+                    </button>
                   </div>
                 </div>
-              </aside>
-              <div className={styles.price}>
-                <span>R$ 100,12</span>
               </div>
             </aside>
-            <div className={styles.separator} />
-          </>
+            <div className={styles.price}>
+            <span>R$ {formatNumber(coffee.price * coffee.amount)}</span>
+            </div>
+          </aside>
+          <div className={styles.separator} />
+        </>
         ))}
       </main>
       <footer>
-          <button className={styles.confirmOrder}>
+          <div className={styles.footerContent}>
+            <div className={styles.infoItem}>
+              <span>Total de itens</span>
+              <span>R$ {formatNumber(totalPrice)}</span>
+            </div>
+            <div className={styles.infoItem}>
+              <span>Entrega</span>
+              <span>R$ {formatNumber(deliveryValue)}</span>
+            </div>
+
+            <div className={styles.infoItem}>
+              <span className={styles.total}>Total</span>
+              <span className={styles.total}>R$ {formatNumber(totalPrice + deliveryValue)}</span>
+            </div>
+          </div>
+          <button className={styles.buttonConfirm}>
             <span>Confirmar Pedido</span>
           </button>
       </footer>
