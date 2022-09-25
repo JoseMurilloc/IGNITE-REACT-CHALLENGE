a54434cb7151ae9ddb44e6f88db857946583fb1b
José Murillo <jooseemurillo@gmail.com>
2022-09-25 13:26:45 -0300

    (coffee) add service to create new buy/cart anywhere

--- coffee-delivary/src/services/Coffee.ts
+++ coffee-delivary/src/services/Coffee.ts
@@ -1,3 +1,20 @@
+import { CoffeeDTO } from '../dtos/Coffee'
+
+interface ClientFormData {
+  cep: string
+  city: string
+  complement: string
+  district: string
+  number: string
+  paymentType: string
+  street: string
+}
+
+interface CreateBuyCoffee {
+  coffees: Array<CoffeeDTO>
+  infoClient: ClientFormData
+}
+
 export class CoffeeService {
   static async getCoffees() {
     try {
@@ -7,6 +24,29 @@ export class CoffeeService {
       return {
         ok: true,
         coffees: json,
+      }
+    } catch (err) {
+      return {
+        ok: false,
+        message: 'Error na requisição de tarefas',
+      }
+    }
+  }
+
+  static async createBuyCoffee(data: CreateBuyCoffee) {
+    try {
+      await fetch('http://localhost:3333/carts', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify({ ...data }),
+      })
+
+      console.log({ data })
+
+      return {
+        ok: true,
       }
     } catch (err) {
       return {
