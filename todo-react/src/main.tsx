12cf1d04bf3300a7a861aebf5d5728b1a7752a6f
José Murillo <jooseemurillo@gmail.com>
2022-11-14 18:01:31 -0300

    (todo) add provider wrapper between app

--- todo-react/src/main.tsx
+++ todo-react/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App'
 import './styles/global'
 import './index.css'
+import { TaskContextProvider } from './contexts/task'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TaskContextProvider>
+      <App />
+    </TaskContextProvider>
   </React.StrictMode>,
 )
